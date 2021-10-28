838197a435e987e93e9b3836c56d27a3132bb120
Masibulele22 <41905558+Masibulele22@users.noreply.github.com>
2021-10-28 15:35:55 +0200

    Added new javascript files

---
+++ js/faqs.js
@@ -0,0 +1,30 @@
+var homeNavbarButton = document.getElementById("homeNavbar");
+homeNavbarButton.style.backgroundColor = "red";
+
+var aboutNavbarButton = document.getElementById("aboutNavbar");
+aboutNavbarButton.style.backgroundColor = "orange";
+
+var centersNavbarButton = document.getElementById("centersNavbar");
+centersNavbarButton.style.backgroundColor = "yellow";
+
+var registerNavbarButton = document.getElementById("registerNavbar");
+registerNavbarButton.style.backgroundColor = "green";
+
+var contactNavbarButton = document.getElementById("contactNavbar");
+contactNavbarButton.style.backgroundColor = "blue";
+
+
+var homeNavbarButton = document.getElementById("homeNavbar");
+homeNavbarButton.style.color = "skyblue";
+
+var aboutNavbarButton = document.getElementById("aboutNavbar");
+aboutNavbarButton.style.color = "skyblue";
+
+var centersNavbarButton = document.getElementById("centersNavbar");
+centersNavbarButton.style.color = "skyblue";
+
+var registerNavbarButton = document.getElementById("registerNavbar");
+registerNavbarButton.style.color = "skyblue";
+
+var contactNavbarButton = document.getElementById("contactNavbar");
+contactNavbarButton.style.color = "skyblue";
---
+++ js/home.js
@@ -0,0 +1,53 @@
+
+// homeNavbarButton.addEventListener('click', function() {
+//     // alert("hello!");
+//     homeNavbarButton.style.backgroundColor = "red";
+// });
+
+// function myFunction() {
+//     return document.getElementById("homeNavbar").style.backgroundColor = "yellow";
+// };
+
+var homeNavbarButton = document.getElementById("homeNavbar");
+
+function myFunction() {
+    homeNavbarButton.style.backgroundColor = "skyblue";
+    homeNavbarButton.style.borderRadius = "10%";
+
+};
+
+
+// function homeNavbarButton(homeNavbarButton) {
+//     return homeNavbarButton.style.backgroundColor = "red";
+// };
+
+// homeNavbarButton();
+
+
+// var aboutNavbarButton = document.getElementById("aboutNavbar");
+// aboutNavbarButton.style.backgroundColor = "orange";
+
+// var centersNavbarButton = document.getElementById("centersNavbar");
+// centersNavbarButton.style.backgroundColor = "yellow";
+
+// var registerNavbarButton = document.getElementById("registerNavbar");
+// registerNavbarButton.style.backgroundColor = "green";
+
+// var contactNavbarButton = document.getElementById("contactNavbar");
+// contactNavbarButton.style.backgroundColor = "blue";
+
+
+// var homeNavbarButton = document.getElementById("homeNavbar");
+// homeNavbarButton.style.color = "skyblue";
+
+// var aboutNavbarButton = document.getElementById("aboutNavbar");
+// aboutNavbarButton.style.color = "skyblue";
+
+// var centersNavbarButton = document.getElementById("centersNavbar");
+// centersNavbarButton.style.color = "skyblue";
+
+// var registerNavbarButton = document.getElementById("registerNavbar");
+// registerNavbarButton.style.color = "skyblue";
+
+// var contactNavbarButton = document.getElementById("contactNavbar");
+// contactNavbarButton.style.color = "skyblue";
