51320913f62d4e16244246ae4987c0b1eb0d4add
nathaneverts <92030626+nathaneverts@users.noreply.github.com>
2021-10-29 14:29:54 +0200

    Add files via upload

--- js/centers.js
+++ js/centers.js
@@ -1,30 +1,6 @@
-var homeNavbarButton = document.getElementById("homeNavbar");
-var aboutNavbarButton = document.getElementById("aboutNavbar");
-var centersNavbarButton = document.getElementById("centersNavbar");
-var registerNavbarButton = document.getElementById("registerNavbar");
-var contactNavbarButton = document.getElementById("contactNavbar");
-
-function homeColorChange() {
-    homeNavbarButton.style.backgroundColor = "red";
-    homeNavbarButton.style.borderRadius = "10%";
-};
-
-function aboutColorChange() {
-    aboutNavbarButton.style.backgroundColor = "orange";
-    aboutNavbarButton.style.borderRadius = "10%";
-};
-
-function centersColorChange() {
-    centersNavbarButton.style.backgroundColor = "yellow";
-    centersNavbarButton.style.borderRadius = "10%";
-};
-
-function registerColorChange() {
-    registerNavbarButton.style.backgroundColor = "green";
-    registerNavbarButton.style.borderRadius = "10%";
-};
-
-function contactColorChange() {
-    contactNavbarButton.style.backgroundColor = "blue";
-    contactNavbarButton.style.borderRadius = "10%";
-};
+// Carousel Auto-Cycle
+$(document).ready(function() {
+  $('.carousel').carousel({
+    interval: 6000
+  })
+});
