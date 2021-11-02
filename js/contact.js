b20e12a5a033f36ee1d6c7bb53ecdfc7c63379ed
Sandamnotoza <90473088+Sandamnotoza@users.noreply.github.com>
2021-11-02 09:36:09 +0200

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -1,30 +1,76 @@
-var homeNavbarButton = document.getElementById("homeNavbar");
-var aboutNavbarButton = document.getElementById("aboutNavbar");
-var centersNavbarButton = document.getElementById("centersNavbar");
-var registerNavbarButton = document.getElementById("registerNavbar");
-var contactNavbarButton = document.getElementById("contactNavbar");
+const form = document.getElementById("form1");
+const fullname = document.getElementById("fullname");
+const email = document.getElementById("email");
+const phonenumber = document.getElementById("phonenumber");
 
-function homeColorChange() {
-    homeNavbarButton.style.backgroundColor = "red";
-    homeNavbarButton.style.borderRadius = "10%";
+form.addEventListener("submit", (e) => {
+    // let message = []
+e.preventDefault();
+
+checkInputs();
+});
+
+function checkInputs(){
+    const fullnameValue = fullname.value.trim();
+    const emailValue = email.value.trim();
+    const phonenumberValue = phonenumber.value.trim();
+    varInput = true;
+    
+    if(fullnameValue === ''){
+        setErrorFor(fullname, "Full name cannot be blank");
+
+    } else {
+        setSuccessFor(fullname)
+    }
+
+    
+    if(emailValue === ''){
+        setErrorFor(email, "Email cannot be blank");
+
+    } else if(!isEmail(emailValue)){
+        setErrorFor(email, "Email is not valid");
+
+    } else {
+        setSuccessFor(email);
+    }
+
+
+    // if(phonenumberValue === ''){
+    //     setErrorFor(phoneNumber, "Phone number cannot be blank");
+
+    // } else if(!isphonenumber(phonenumberValue)|| phonenumberValue.length > 10 || phonenumberValue.length < 10){
+    //     setErrorFor(phonenumber, "Phone Number is not valid");
+
+    // }else {
+    //     setSuccessFor(phonenumber);
+    // }
+
 };
 
-function aboutColorChange() {
-    aboutNavbarButton.style.backgroundColor = "orange";
-    aboutNavbarButton.style.borderRadius = "10%";
-};
+function setErrorFor(input, message){
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
 
-function centersColorChange() {
-    centersNavbarButton.style.backgroundColor = "yellow";
-    centersNavbarButton.style.borderRadius = "10%";
-};
+    small.innerText = message;
 
-function registerColorChange() {
-    registerNavbarButton.style.backgroundColor = "green";
-    registerNavbarButton.style.borderRadius = "10%";
+    formControl.className = 'inputbox error';
 };
+function setSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'inputbox success';
+};
+function isEmail (email) {
+    const re =  /^[a-zA-Z0-9.! #*+/=? ^_{|}~-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*$/;
+    return re.test(email);
+};
+// function isphonenumber (phonenumber) {
 
-function contactColorChange() {
-    contactNavbarButton.style.backgroundColor = "blue";
-    contactNavbarButton.style.borderRadius = "10%";
-};
+//     const re = /^(0)?[6-8][0-9]{9}/;
+//     return re.test(phonenumber);
+//     // /^[\d,\s, \+,-]{5,20}/
+//     // /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}/;
+// };
+
+
+
+
